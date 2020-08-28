662e8dbece14610aa6ca24517e9821e7e5d92da1
Matt Proud <proudmatt@gmail.com>
2020-08-28 16:25:38 +0100

    add vds 7 hack

--- tasks/pave-vsphere-ansible/task.sh
+++ tasks/pave-vsphere-ansible/task.sh
@@ -7,6 +7,9 @@ export ROOT_DIR="$(pwd)"
 mkdir -v tmp
 VC_ISO="$(ls -d ${ROOT_DIR}/vcenter-iso/*.iso)"
 
+# Run hack to all creation of VDS 7.0. Will be fixed in Ansible 2.10
+${ROOT_DIR}/vsphere-ansible-repo/fix_dvs_for_7.sh
+
 echo "Using ISO ${VC_ISO}"
 ansible-playbook "${ROOT_DIR}/vsphere-ansible-repo/deploy.yml" \
     --extra-vars="@${ROOT_DIR}/config/${VSPHERE_ANSWERS}" \
