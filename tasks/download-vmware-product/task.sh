621fbcc2f421d236591011792d3b1ad5c512582e
Matt Proud <proudmatt@gmail.com>
2020-10-25 09:56:54 +0000

    add vmware download task

---
+++ tasks/download-vmware-product/task.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -euxo pipefail
+
+ROOT_DIR=$(pwd)
+cp downloaded-product
+
+PRODUCT_FILES="$(vmw-cli ls ${PRODUCT_DIRECTORY})"
+
+FILE_NAME="$(echo "${PRODUCT_FILES}" |grep ${FILE_GLOB} |cut -d " " -f1)"
+
+if [ `echo "${FILE_NAME}" |wc -l` -ge 2 ]
+then
+    echo "The glob must only return 1 result"
+    echo "The following results were returned: \n${FILE_NAME}"
+    echo "Exiting"
+    exit 1
+fi
+
+vmw-cli cp "${FILE_NAME}"
