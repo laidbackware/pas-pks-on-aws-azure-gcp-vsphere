f06eea5887a40fb3167f3152ad0d7b8d1471b8e3
Matt Proud <proudmatt@gmail.com>
2020-11-03 10:37:14 +0000

    refactor vsphere deploy to use new repo

--- tasks/pave-vsphere-ansible/task.sh
+++ tasks/pave-vsphere-ansible/task.sh
@@ -6,12 +6,10 @@ export ROOT_DIR="$(pwd)"
 
 mkdir -v tmp
 VC_ISO="$(ls -d ${ROOT_DIR}/vcenter-iso/*.iso)"
-
-# Run hack to all creation of VDS 7.0. Will be fixed in Ansible 2.10
-${ROOT_DIR}/vsphere-ansible-repo/fix_dvs_for_7.sh
+ESXI_OVA="$(ls -d ${ROOT_DIR}/esxi-ova/*.ova)"
 
 echo "Using ISO ${VC_ISO}"
 ansible-playbook "${ROOT_DIR}/vsphere-ansible-repo/deploy.yml" \
     --extra-vars="@${ROOT_DIR}/config/${VSPHERE_ANSWERS}" \
-    --extra-vars "tmp_dir=${ROOT_DIR}/tmp vcIso=${VC_ISO} \
+    --extra-vars "tmp_dir=${ROOT_DIR}/tmp vcIso=${VC_ISO} esxi_ova=${ESXI_OVA} \
     environment_tag=${FOUNDATION}"
