9e9aa163856786d2f5c11b522f417cee45633c65
Matt Proud <proudmatt@gmail.com>
2020-11-03 11:03:04 +0000

    tidy up secrets and remove yq from install pipeline

--- tasks/paving-terraform/task.sh
+++ tasks/paving-terraform/task.sh
@@ -7,45 +7,26 @@ ROOT_DIR="$(pwd)"
 # Setup tools
 cp ${ROOT_DIR}/terraform/terraform-* /usr/local/bin/terraform
 chmod +x /usr/local/bin/terraform 
-# ${ROOT_DIR}/config/lib/add-tools/setup-oq.sh
 
 if [ ${IAAS_TYPE} = "vsphere" ]; then
     NSX_VAR_FILE=${ROOT_DIR}/config/vars/${FOUNDATION}/nsxt-answerfile.yml
     if [ ! -f $NSX_VAR_FILE ]; then echo "${NSX_VAR_FILE} File not found!"; fi
     export TF_VAR_nsxt_host="$(bosh int $NSX_VAR_FILE  --path /nsx_manager/hostname)"
-    export TF_VAR_nsxt_username="$(bosh int <(echo ${CLOUD_CREDS}) --path /client_id)"
-    export TF_VAR_nsxt_password="$(bosh int <(echo ${CLOUD_CREDS}) --path /client_secret)"
+    export TF_VAR_nsxt_username="$(bosh int <(echo ${CLOUD_CREDS}) --path /nsx_username)"
+    export TF_VAR_nsxt_password="$(bosh int <(echo ${CLOUD_CREDS}) --path /nsx_password)"
     export TF_VAR_overlay_transport_zone_name="$(bosh int ${NSX_VAR_FILE} --path /transportzones/0/display_name)"
     export TF_VAR_vlan_transport_zone_name="$(bosh int ${NSX_VAR_FILE} --path /transportzones/1/display_name)"
     export TF_VAR_nsxt_edge_cluster_name="$(bosh int ${NSX_VAR_FILE} --path /edge_cluster_name)"
-    # export TF_VAR_nsxt_t0_router_name="$(bosh int ${NSX_VAR_FILE} --path /logical_routers/0/router_display_name)"
-    # export TF_VAR_nsxt_host="$(oq -i yaml .nsx_manager.hostname $NSX_VAR_FILE | tr -d '"')"
-    # export TF_VAR_nsxt_username="$(echo ${CLOUD_CREDS} | oq .client_id | tr -d '"' )"
-    # export TF_VAR_nsxt_password="$(echo ${CLOUD_CREDS} | oq .client_secret | tr -d '"' )"
-    # export TF_VAR_overlay_transport_zone_name="$(oq -i yaml .transportzones[0].display_name $NSX_VAR_FILE | tr -d '"')"
-    # export TF_VAR_nsxt_edge_cluster_name="$(oq -i yaml .edge_cluster_name $NSX_VAR_FILE | tr -d '"')"
-    # export TF_VAR_nsxt_t0_router_name="$(oq -i yaml .logical_routers[0].router_display_name $NSX_VAR_FILE | tr -d '"')"
     cd ${ROOT_DIR}/config/terraform/nsxt
 else # Setup for all cloud providers
     cd ${ROOT_DIR}/config/terraform/nsxt
-    # cp ${ROOT_DIR}/config/terraform/backend.tf .
-    # EXTRA_TF_OUTPUTS='
-    # output "stable_config_yaml" {
-    #     value     = yamlencode(local.stable_config)
-    #     sensitive = true
-    # }'
-    # echo "${EXTRA_TF_OUTPUTS}" >> outputs.tf
     export TF_VAR_access_key="$(bosh int <(echo ${CLOUD_CREDS}) --path /client_id)"
     export TF_VAR_secret_key="$(bosh int <(echo ${CLOUD_CREDS}) --path  /client_secret)"
-    # export TF_VAR_access_key="$(echo ${CLOUD_CREDS} | oq .client_id | tr -d '"' )"
-    # export TF_VAR_secret_key="$(echo ${CLOUD_CREDS} | oq .client_secret | tr -d '"' )"
 fi
 
 if [ ${FOUNDATION} = "azure" ]; then
     export TF_VAR_subscription_id="$(bosh int <(echo ${CLOUD_CREDS}) --path /subscription_id)"
     export TF_VAR_tenant_id="$(bosh int <(echo ${CLOUD_CREDS}) --path /tenant_id)"
-    # export TF_VAR_subscription_id="$(echo ${CLOUD_CREDS} | oq .subscription_id | tr -d '"' )"
-    # export TF_VAR_tenant_id="$(echo ${CLOUD_CREDS} | oq .tenant_id | tr -d '"' )"
 fi
 
 terraform init -backend-config="bucket=$STATE_BUCKET" \
--- tasks/wipe-paving-terraform/task.sh
+++ tasks/wipe-paving-terraform/task.sh
@@ -12,7 +12,6 @@ ROOT_DIR="$(pwd)"
 # Setup tools
 cp ${ROOT_DIR}/terraform/terraform-* /usr/local/bin/terraform
 chmod +x /usr/local/bin/terraform
-# ${ROOT_DIR}/config/lib/add-tools/setup-oq.sh
 
 ENV_FILE=vars/${FOUNDATION}/env/env.yml
 OM_TARGET=$(bosh int config/${ENV_FILE} --path /target | sed s"/((domain))/${DOMAIN}/")
@@ -25,19 +24,12 @@ fi
 
 cd ${ROOT_DIR}/paving-repo/${FOUNDATION}
 cp ${ROOT_DIR}/config/terraform/backend.tf .
-# echo "${EXTRA_TF_OUTPUTS}" >> outputs.tf
 export TF_VAR_access_key="$(bosh int <(echo ${CLOUD_CREDS}) --path /client_id)"
 export TF_VAR_secret_key="$(bosh int <(echo ${CLOUD_CREDS}) --path /client_secret)"
-
-# export TF_VAR_access_key="$(echo ${CLOUD_CREDS} | oq .client_id | tr -d '"' )"
-# export TF_VAR_secret_key="$(echo ${CLOUD_CREDS} | oq .client_secret | tr -d '"' )"
-
 
 if [ ${FOUNDATION} = "azure" ]; then
     export TF_VAR_subscription_id="$(bosh int <(echo ${CLOUD_CREDS}) --path /subscription_id)"
     export TF_VAR_tenant_id="$(bosh int <(echo ${CLOUD_CREDS}) --path /tenant_id)"
-    # export TF_VAR_subscription_id="$(echo ${CLOUD_CREDS} | oq .subscription_id | tr -d '"' )"
-    # export TF_VAR_tenant_id="$(echo ${CLOUD_CREDS} | oq .tenant_id | tr -d '"' )"
 fi
 
 terraform init -backend-config="bucket=${STATE_BUCKET}" \
